7f75b8a200d5f7d61e4131aa15a5ae0f0654a072
“Osaro <saro.iyoha@gmail.com>
2025-06-11 22:28:45 +0100

    add Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,21 @@
+import './Hero.css'
+import Navbar from './Navbar'
+function Hero() {
+  return (
+
+    <div className="hero">
+        <Navbar />
+        <section className="hero-container">
+            <div className="hero-container__text">
+                <h1>Exquisite dining since 1989</h1>
+            <p>Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.</p>
+            <button className="cta-button">Book a table</button>
+            </div>
+            <div className="hero-container__image"></div>
+            
+        </section>
+    </div>
+  )
+}
+
+export default Hero
