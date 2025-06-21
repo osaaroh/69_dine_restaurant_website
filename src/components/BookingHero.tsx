49b3082364973fc7f1f75b09d37e4fd945263111
“Osaro <saro.iyoha@gmail.com>
2025-06-21 23:09:17 +0100

    update Booking.tsx

--- src/components/BookingHero.tsx
+++ src/components/BookingHero.tsx
@@ -1,6 +1,6 @@
-import './BookingHero.css'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import './BookingHero.css'
 function BookingHero() {
   return (
 <>
@@ -11,6 +11,52 @@ function BookingHero() {
             <div className="hero-container__text">
                 <h1>Reservations</h1>
             <p>We can’t wait to host you. If you have any special requirements please feel free to call on the phone number below. We’ll be happy to accommodate you.</p>
+            </div>
+            <div className="reservation__form">
+                <div className="form-group">
+            {/* <label htmlFor="fullname">Full Name</label> */}
+            <span className="input-wrapper">
+              <input type="text" id="fullname" placeholder='Name'/>
+            </span>
+            <span className="error">
+              <p>This field is required</p>
+            </span>
+          </div>
+          <div className="form-group">
+            {/* <label htmlFor="email-address">Email Address</label> */}
+            <span className="input-wrapper">
+              <input
+                type="text"
+                id="email-address"
+                placeholder="Email"
+              />
+            </span>
+            <span className="error">
+              <p>This field is required</p>
+            </span>
+          </div>
+
+          <div className="form-group">
+
+            <span className="input-wrapper">
+              <input
+                type="text"
+                id="email-address"
+                placeholder="Email"
+              />
+            </span>
+            <span className="error">
+              <p>This field is required</p>
+            </span>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="submit-button" className="input-wrapper button-wrapper">
+              <button id="submit-button"  type="submit">
+                Make Reservation
+              </button>
+            </label>
+          </div>
             </div>
         </section>
       </div>
